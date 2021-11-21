22564aadbd4f3ad6e803472a99b668ac8e5c4091
chintyaw <cwchintya@gmail.com>
2021-11-21 12:37:44 +0700

    change string format

--- assets/bootstrap/js/d3.js
+++ assets/bootstrap/js/d3.js
@@ -53,7 +53,7 @@ function bpm(email) {
 
     //Read the data
     // When reading the csv, I must format variables:
-    d3.csv("https://backend-embedded-system.herokuapp.com/csv/{email}",
+    d3.csv("https://backend-embedded-system.herokuapp.com/csv/${email}",
       
     function(d){
       return { date : d3.timeParse("%Y-%m-%d %H:%M:%S")(d.date), value : d.bpm }
@@ -178,7 +178,7 @@ function spo2(email) {
 
     //Read the data
     // When reading the csv, I must format variables:
-    d3.csv("https://backend-embedded-system.herokuapp.com/csv/{email}",
+    d3.csv("https://backend-embedded-system.herokuapp.com/csv/${email}",
       
     function(d){
       return { date : d3.timeParse("%Y-%m-%d %H:%M:%S")(d.date), value : d.spo2 }
